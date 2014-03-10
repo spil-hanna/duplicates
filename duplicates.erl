92b837e2b381226a5aa0f65372933f979cf4a423
Hanna Kollo <Hanna.Kollo@spilgames.com>
2014-03-10 09:23:13 +0100

    initial

---
+++ duplicates.erl
@@ -0,0 +1,7 @@
+- module(duplicates).
+
+% remove_duplicates([term()]) -> [term()]
+% with the property that the order of first occurrences is preserved.
+% EG: remove_duplicates([d,a,b,a,c,b,c,a]) = [d,a,b,c] (any other order is not good)
+
+
